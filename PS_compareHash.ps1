5ecbd5ea8949b3d0e09e470a6e00f018e601e592
Köthe <NWO666@oper2003.staatstheater.sthh>
2017-05-22 13:27:58 +0200

    FolderUpdate

--- PS_compareHash.ps1
+++ PS_compareHash.ps1
@@ -1,5 +1,5 @@
-$LeftFolder = ""
-$RightFolder = ""
+$LeftFolder = "sf"
+$RightFolder = "df"
 $LeftSideHash = Get-ChildItem $LeftFolder -Recurse | Get-FileHash | select @{Label="Path";Expression={$_.Path.Replace($LeftFolder,"")}},Hash
 $RightSideHash = Get-ChildItem $RightFolder -Recurse | Get-FileHash | select @{Label="Path";Expression={$_.Path.Replace($RightFolder,"")}},Hash
 Compare-Object $LeftSideHash $RightSideHash -Property Path,Hash
